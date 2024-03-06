a6c31e8e59df9126fefe3c0bab80bda6fe56d706
YounessBoumlik <younessboumlik1949@gmail.com>
2024-03-06 18:30:16 +0100

    adding things

---
+++ Project/index.js
@@ -0,0 +1,20 @@
+const mysql1 = require('mysql')
+
+const db = mysql1.createConnection({
+    host:'http://sql11.freesqldatabase.com',
+    user : 'sql11688952',
+    password : 'BTDEuLF25K',
+    database : 'sql11688952',
+    port:'3306'
+})
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to the database.');
+
+    let sql = 'SELECT * FROM USER'; // Replace with your SQL query
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+    });
+});
