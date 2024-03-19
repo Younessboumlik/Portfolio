12d0a324a98c9ecffee807cc930cbf94e66eb409
YounessBoumlik <younessboumlik1949@gmail.com>
2024-03-19 13:18:30 +0000

    update

---
+++ dbpythontest.py
@@ -0,0 +1,13 @@
+import mysql.connector as mysql
+
+# mysql.connect{
+#     host="localhost",
+#     user="yourusername",
+#     password="yourpassword"
+#     # host = "sql11.freesqldatabase.com"
+#     # "name" : "sql11688952"
+# # Nom de la base de données : sql11688952
+# # Utilisateur de la base de données : sql11688952
+# # Mot de passe de la base de données : BTDEuLF25K
+# # Numéro de port : 3306
+# }
