68bb25a0b1ff36b9a72c2c4aa91292643a6d0950
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-03-20 03:37:32 +0000

    hhhhhhhhhhh

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
-// let a = require('mysql')
+let a = require('mysql')
 let exp = require("express")
+const bodyParser = require('body-parser');
 let email = undefined;
 let id = undefined ;
-function Query(){
-b = a.createConnection({host :'sql11.freesqldatabase.com' ,  user: "sql11688952" , password:"BTDEuLF25K" ,port :"3306"})
-b.connect(function(err) {
+query_result = undefined
+// function Query(){
+db = a.createConnection({host :"sql11.freesqldatabase.com" ,  user: "sql11692837" , password:"ESZ2YTvzKy", port:"3306"})
+db.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
 });
-b.query("use sql11688952;", function(err) {
+db.query("USE sql11692837" , function(err){
     if (err) throw err;
-});
-b.query("select * from Users;", function(err, result) {
-    if (err) throw err;
-    console.log("Result:", result);
-})};
-// const fun = (req,res) => {console.log(req.body.email)
-//     res.sendFile("./views/contact.html",{root : __dirname})}
+})
 const app = exp();
-const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(exp.static(__dirname + '/public'));
+app.use(exp.static('public'));
 app.set("view engine","ejs");
 // parse application/json
 app.use(bodyParser.json());
@@ -33,16 +29,27 @@ app.get('/login' ,(req,res) => {
 
 app.post('/home/me' ,(req,res) => {console.log(req.body.email)
     email = req.body.email;
-    // b.query(`select * from Users where email = ${email} and ;`, function(err, result) {
-    //     if (err) throw err;
-    //     console.log("Result:", result);})
-    console.log(email)
-
-    res.sendFile("./Project/homeaftersignin.html",{root : __dirname})})
+    if (req.body.submit === "creatacc"){
+    db.query(`select * from Users where email = ?;`,[email], function(err, result) {
+        if (err) throw err;
+        query_result = result
+        if (query_result.length === 0){
+            db.query(`insert into Users (email,password,first_name,last_name) values (?, ?, ?, ?)`, [req.body.email, req.body.password, req.body.first_name, req.body.last_name],
+            function(err){
+                res.sendFile("./Project/homeaftersignin.html",{root : __dirname})
+            })
+            
+         }
+       else{
+           res.render("createaccount",{checkcreataccount:true})
+         }})
+    
+    }
+ })
 app.get('/inscription', (req,res) => {
     if (email === undefined){
       res.sendFile('./views/gologin.html',{root : __dirname})
-    }
+    } 
     else {
         res.sendFile("./Project/inscription1.html",{root : __dirname})
     }
@@ -52,12 +59,18 @@ app.get('/inscription/etudiant' ,(req,res) => {
 
 })
 app.get('/createaccount' ,(req,res) => {
-    res.sendFile("./Project/createaccount.html",{root : __dirname})
+    res.render("createaccount",{checkcreataccount:false})
 
 })
 app.get('/profile' ,(req,res) => {
     res.render("profile",{mail:email})
 
 })
+app.get('/services' ,(req,res) => {
+    res.sendFile("./Project/services.html",{root : __dirname})
+})
+app.get('/courses' ,(req,res) => {
+    res.sendFile("./Project/courses.html",{root : __dirname})
+})
 
-app.listen(2299,() => (console.log("https://localhost:2228")))
+app.listen(9222,() => (console.log("https://localhost:2228")))
